6f56e5d64630a6e51017ddbae517fef737c4b44b
ErickCassoli <erick.mcassoli@gmail.com>
2024-03-15 16:05:26 -0300

    feat: create models

---
+++ II/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Books(models.Model):
+    sensor = models.BooleanField(default=False)
+    botao = models.BooleanField(default=False)
+    ligaRobo = models.BooleanField(default=False)
+    resetContador = models.BooleanField(default=False)
+    valorContagem = models.IntegerField(default=0)
